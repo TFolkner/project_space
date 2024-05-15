4286e9c80222bd904afb61c295841e3da0ca8669
Folkner <you@example.com>
2024-05-16 00:19:04 +0300

    Начал перестройку

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 # Тестовый телеграм эхо бот, запщенный на сервере с гитхаба
 # Тестовый комит
-
-
-from aiogram import Bot, Dispatcher, types, executor # type: ignore
 # для aiogram pip install aiogram==2.25.2
 
+import asyncio
+import logging
 
-API_TOKEN = '7061565788:AAG-e1jFINrhKbBLoXs5Pg_YJokM634j740'
-bot = Bot (token = API_TOKEN)
-dp = Dispatcher (bot)
+from aiogram import Bot, Dispatcher # type: ignore 
+from aiogram.enums.parse_mode import ParseMode # type: ignore 
+from aiogram.fsm.storage.memory import MemoryStorage # type: ignore 
+
+import config # type: ignore 
+from handlers import router # type: ignore 
 
 
-@dp.message_handler(commands=['start']) #Явно указываем в декораторе, на какую команду реагируем. 
-async def send_welcome(message: types.Message):
-   await message.reply("Привет,я тестовый бот проекта \"КОСМОС\"\n Тут будут тестироваться новые функции, приятного использования\n :-)") 
-   #Так как код работает асинхронно, то обязательно пишем await.
 
 
-#Создаём новое событие, которое запускается в ответ на любой текст, введённый пользователем.
-@dp.message_handler() 
-async def echo(message: types.Message): 
-   #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
-   await message.answer(message.text)
 
+def read_data (sFile_data):
+	global API_TOKEN
+	pFile_data = open (sFile_data, "r")
+	API_TOKEN = pFile_data.readline ()
+	return 0
+
+
+
+
+
+def main ():
+	read_data ("data.txt")
+	return 0
 
 
 if __name__ == '__main__':
-   executor.start_polling(dp, skip_updates=True)
+	main()
